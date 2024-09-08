e26f0a7dfb2e839c5423b8d92b56919c733ec9d2
Franco Astegiano <f.astegiano@gmail.com>
2024-09-08 12:44:02 -0300

    mvp

--- src/provinciamascercana.py
+++ src/provinciamascercana.py
@@ -1,4 +1,5 @@
 #pip install Levenshtein
+
 """
 import provinciamascercana as pmc
 provincia_cercana = pmc.encontrar_provincia_mas_cercana(texto)
@@ -14,8 +15,6 @@ import unicodedata
 from Levenshtein import distance as levenshtein_distance
 
 
-
-
 def normalizar_texto(texto):
     
     """
@@ -26,22 +25,22 @@ def normalizar_texto(texto):
     return texto_normalizado.upper()  # Convertir a mayúsculas
 
 
-def encontrar_provincia_mas_cercana(texto,umbral=3):
+def encontrar_provincia_mas_cercana(texto, locations,umbral=3):
     """
     Encuentra la provincia más cercana a partir del texto ingresado utilizando la distancia de Levenshtein.
     
     """
-    provincias = ["BUENOS AIRES", "CORDOBA", "SANTA FE", "MENDOZA", "SAN JUAN", "SAN LUIS","CATAMARCA","LA RIOJA","SANTA CRUZ","FORMOSA","CHUBUT","CABA","ENTRE RIOS","LA PAMPA","NEUQUEN","RIO NEGRO","MISIONES","CORRIENTES","CHACO","TUCUMAN","SALTA","JUJUY","TIERRA DEL FUEGO"]
+    #provincias = ["BUENOS AIRES", "CORDOBA", "SANTA FE", "MENDOZA", "SAN JUAN", "SAN LUIS","CATAMARCA","LA RIOJA","SANTA CRUZ","FORMOSA","CHUBUT","CABA","ENTRE RIOS","LA PAMPA","NEUQUEN","RIO NEGRO","MISIONES","CORRIENTES","CHACO","TUCUMAN","SALTA","JUJUY","TIERRA DEL FUEGO"]
     texto_normalizado = normalizar_texto(texto)
     mejor_coincidencia = None
     menor_distancia = float('inf')
 
-    for provincia in provincias:
-        provincia_normalizada = normalizar_texto(provincia)
-        distancia = levenshtein_distance(texto_normalizado, provincia_normalizada)
+    for loc in locations:
+        loc_normalizada = normalizar_texto(loc)
+        distancia = levenshtein_distance(texto_normalizado, loc_normalizada)
         if distancia < menor_distancia and distancia <= umbral:
             menor_distancia = distancia
-            mejor_coincidencia = provincia
+            mejor_coincidencia = loc
 
     return mejor_coincidencia
 
